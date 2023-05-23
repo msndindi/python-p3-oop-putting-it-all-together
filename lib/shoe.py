71fec4c3e41af9c1364b382ae4bb6fc8e8c03f5d
Mutindi Hope <msndindi@outlook.com>
2023-05-23 08:45:45 +0300

    Completed Assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 class Shoe:
     pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size
+        self.condition = None
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    size = property(get_size, set_size)
